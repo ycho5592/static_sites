507b9f9c964b3fa32a6034b59676de229ea3a3ea
ycho5592 <116114988+ycho5592@users.noreply.github.com>
2026-02-09 16:09:47 -0500

    copy static

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
-# ./main.sh
-from textnode import TextNode, TextType
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+
+source_path = "./static"
+dest_path = "./public"
 
 def main():
-    node = TextNode("This is some anchor text", TextType.BOLD, "htts://www.boot.dev")
-    print(node)
+    
+    print("Cleaning public directory...")
+    if os.path.exists(dest_path):
+        shutil.rmtree(dest_path)
+    
+    print("Copying static files to public...")
+    copy_files_recursive(source_path, dest_path)
 
 if __name__ == "__main__":
     main()
