b5c4ae2a85577f48f7caba9937cbd431670b73da
ycho5592 <116114988+ycho5592@users.noreply.github.com>
2026-02-09 09:16:09 -0500

    updated textnode

--- src/main.py
+++ src/main.py
@@ -1,2 +1,10 @@
-./main.sh
-print("hello world")
+# ./main.sh
+from textnode import TextNode, TextType
+
+def main():
+    node = TextNode("This is some anchor text", TextType.BOLD, "htts://www.boot.dev")
+    print(node)
+
+if __name__ == "__main__":
+    main()
+
